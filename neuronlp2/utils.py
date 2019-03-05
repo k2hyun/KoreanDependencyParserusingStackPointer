fix error while normalizing digit word for python3.6

neuronlp2/utils.py
@@ -39,7 +39,7 @@ def load_embedding_dict(embedding, embedding_path, normalize_digits=True):
                     assert (embedd_dim + 1 == len(tokens))
                 embedd = np.empty([1, embedd_dim], dtype=np.float32)
                 embedd[:] = tokens[1:]
-                word = utils.DIGIT_RE.sub(b"0", tokens[0]) if normalize_digits else tokens[0]
+                word = utils.DIGIT_RE.sub("0", tokens[0]) if normalize_digits else tokens[0]
                 embedd_dict[word] = embedd
         return embedd_dict, embedd_dim
     elif embedding == 'senna':